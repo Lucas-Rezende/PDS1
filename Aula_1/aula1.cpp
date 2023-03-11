5c2e5cdf7a54b0958b9575e440d52becfdb71fd4
Lucas-Rezende <lucasmomede@yahoo.com.br>
2023-03-11 12:53:40 -0300

    Update Aula_1

--- Aula_1/aula1.cpp
+++ Aula_1/aula1.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 int main (int argc, char *argv[])
 {
-  int a, b, c, d;
+  float a, b, c, d;
 
   //QUESTÃO 1
 
-  /*std::cout << "QUESTÃO 1" << std::endl;
+  std::cout << "QUESTÃO 1" << std::endl;
   std::cout << 3+4 << std::endl;
   std::cout << 7/4 << std::endl;
   std::cout << pow(3,2) << std::endl;
@@ -30,14 +30,14 @@ int main (int argc, char *argv[])
   std::cout << log(2.3)-log(3.1) << std::endl;
   std::cout << log(7)+(log(7)*log(7)-cos(log(7))) << std::endl;
   std::cout << (10*3+8*4)/50.3 - (10.3+8.4) << std::endl;
-  std::cout << (cos(0.8)+sin(0.8)) * (cos(0.8)-sin(0.8)) << std::endl;*/
+  std::cout << (cos(0.8)+sin(0.8)) * (cos(0.8)-sin(0.8)) << std::endl;
 
   //QUESTÃO 2
 
-  std::cout << "QUESTÃO 2" << std::endl;
+  std::cout << "\nQUESTÃO 2" << std::endl;
   std::cout << "Digite os valores a, b, c e d respectivamente: ";
   std::cin >> a >> b >> c >> d;
-  /*std::cout << a+b << std::endl;
+  std::cout << a+b << std::endl;
   std::cout << a/c << std::endl;
   std::cout << pow(a, 2) << std::endl;
   std::cout << b*c << std::endl;
@@ -54,7 +54,7 @@ int main (int argc, char *argv[])
   std::cout << log(a) - log(c) << std::endl;
   std::cout << log(a) + (log(b)*log(d)-cos(log(c))) << std::endl;
   std::cout << (b+a)/c - (d+a) << std::endl;
-  std::cout << (cos(d)+sin(c)*(cos(b)-sin(a))) << std::endl;*/
+  std::cout << (cos(d)+sin(c)*(cos(b)-sin(a))) << std::endl;
 
   //QUESTÃO 3
   std::cout << "\nQUESTÃO 3" << std::endl;
@@ -63,7 +63,7 @@ int main (int argc, char *argv[])
   std::cout << "\nPerimetro (circ.): " << 2*M_PI*a << std::endl;
   std::cout << "\nArea (triang.)   : " << (b*c)/2 << std::endl;
   std::cout << "\nHipotenusa       : " << pow(b, 2) + pow(c, 2) << std::endl;
-  std::cout << "\nRaizes 1 e 2     : " << (-b + sqrt(pow(b, 2)-4*a*c))/2*a << " e " << (-b - sqrt(pow(b, 2)-4*a*c))/2*a << std::endl;
+  std::cout << "\nRaizes 1 e 2     : " << (-b + sqrt(pow(b, 2) - (4*a*c)))/(2*a) << " e " << (-b - sqrt(pow(b, 2) - 4*a*c))/(2*a) << std::endl;
   
   return 0;
 }
